779f10fe2f4b629fe15e10a6c1c0bd7938b439cb
CahenTaylor <cahentaylor@gmail.com>
2023-03-29 11:18:38 -0600

    images displayed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // write your code here
+
+// Declarations
+const ramenMenu = document.querySelector("#ramen-menu");
+const ramenDetails = document.querySelector("#ramen-detail");
+const ratingDisplay = document.querySelector("#rating-display");
+const commentDisplay = document.querySelector("#comment-display");
+const newRamenForm = document.querySelector("#new-ramen");
+
+
+//Fetch requests
+fetch("http://localhost:3000/ramens")
+.then((ramens) => ramens.json())
+.then(ramens => {
+	console.log(ramens);
+    for (const ramen of ramens) {
+        console.log(ramen);
+        const img = document.createElement("img");
+        img.src = ramen.image
+        img.alt = ramen.name
+        ramenMenu.append(img);
+
+    } 
+
+	
+    
+    })
+    
+
+
+
+
+
+
+
+
+//DOM manipulators
+
+
